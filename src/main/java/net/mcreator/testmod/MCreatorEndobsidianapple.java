8311ed340abfe2f8ea662392a99730fa4b14122c
htbrdd <htbrdd@gmail.com>
2020-03-01 21:36:14 -0500

    armor recipes complete. apple created and recipe complete.

---
+++ src/main/java/net/mcreator/testmod/MCreatorEndobsidianapple.java
@@ -0,0 +1,50 @@
+package net.mcreator.testmod;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.item.UseAction;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.Food;
+
+@Elementstestmod.ModElement.Tag
+public class MCreatorEndobsidianapple extends Elementstestmod.ModElement {
+	@ObjectHolder("testmod:endobsidianapple")
+	public static final Item block = null;
+
+	public MCreatorEndobsidianapple(Elementstestmod instance) {
+		super(instance, 8);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new FoodItemCustom());
+	}
+
+	public static class FoodItemCustom extends Item {
+		public FoodItemCustom() {
+			super(new Item.Properties().group(ItemGroup.FOOD).maxStackSize(64)
+					.food((new Food.Builder()).hunger(25).saturation(1f).setAlwaysEdible().meat().build()));
+			setRegistryName("endobsidianapple");
+		}
+
+		@Override
+		public int getUseDuration(ItemStack stack) {
+			return 40;
+		}
+
+		@Override
+		@OnlyIn(Dist.CLIENT)
+		public boolean hasEffect(ItemStack itemstack) {
+			return true;
+		}
+
+		@Override
+		public UseAction getUseAction(ItemStack par1ItemStack) {
+			return UseAction.EAT;
+		}
+	}
+}
