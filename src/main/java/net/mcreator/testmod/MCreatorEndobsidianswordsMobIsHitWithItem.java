b41b2357e4de9787e28fb8d59596bf2265a8b63d
htbrdd <htbrdd@gmail.com>
2020-03-02 20:52:57 -0500

    still working on sword

--- src/main/java/net/mcreator/testmod/MCreatorEndobsidianswordsMobIsHitWithItem.java
+++ src/main/java/net/mcreator/testmod/MCreatorEndobsidianswordsMobIsHitWithItem.java
@@ -1,18 +1,12 @@
 package net.mcreator.testmod;
 
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.event.entity.living.LivingAttackEvent;
-import net.minecraftforge.common.MinecraftForge;
-
-import net.minecraft.world.World;
-import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.DamageSource;
 import net.minecraft.entity.Entity;
 
 @Elementstestmod.ModElement.Tag
 public class MCreatorEndobsidianswordsMobIsHitWithItem extends Elementstestmod.ModElement {
 	public MCreatorEndobsidianswordsMobIsHitWithItem(Elementstestmod instance) {
 		super(instance, 13);
-		MinecraftForge.EVENT_BUS.register(this);
 	}
 
 	public static void executeProcedure(java.util.HashMap<String, Object> dependencies) {
@@ -21,25 +15,6 @@ public class MCreatorEndobsidianswordsMobIsHitWithItem extends Elementstestmod.M
 			return;
 		}
 		Entity entity = (Entity) dependencies.get("entity");
-		entity.setMotionMultiplier(null, new Vec3d(0.25D, (double) 0.05F, 0.25D));
-	}
-
-	@SubscribeEvent
-	public void onEntityAttacked(LivingAttackEvent event) {
-		if (event != null && event.getEntity() != null) {
-			Entity entity = event.getEntity();
-			int i = (int) entity.posX;
-			int j = (int) entity.posY;
-			int k = (int) entity.posZ;
-			World world = entity.world;
-			java.util.HashMap<String, Object> dependencies = new java.util.HashMap<>();
-			dependencies.put("x", i);
-			dependencies.put("y", j);
-			dependencies.put("z", k);
-			dependencies.put("world", world);
-			dependencies.put("entity", entity);
-			dependencies.put("event", event);
-			this.executeProcedure(dependencies);
-		}
+		entity.attackEntityFrom(DamageSource.GENERIC, (float) 4);
 	}
 }
